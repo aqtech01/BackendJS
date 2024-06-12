5820725b692e5e7d4fcc6f0972a4f23390770417
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-06-12 14:15:27 +0000

    ...

---
+++ script.js
@@ -0,0 +1,21 @@
+let xp= 0;
+let health = 100;
+let gold = 50;
+let currentWeapon= 0;
+let fighting;
+let button1 = document.querySelector("#button1")
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goStore(){
+console.log("Going to Store"); 
+
+}
+
+function goCave () {
+console.log("Going to Cave");
+}
+function fightDragon () {
+console.log("Going to Fight Dragon");
+}
